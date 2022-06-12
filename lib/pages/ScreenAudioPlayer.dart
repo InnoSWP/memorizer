6e704f7be1b83785901e40111035e4d7f162f072
kamil <almetov.kamil@gmail.com>
2022-06-12 15:04:12 +0300

    Rename appColors.dart to constants.dart, change all constants names according to Flutter conversion: kThisIsConstant

--- lib/pages/ScreenAudioPlayer.dart
+++ lib/pages/ScreenAudioPlayer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:memorizer/settings/appColors.dart';
+import 'package:memorizer/settings/constants.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class AudioPlayerOur extends StatefulWidget {
