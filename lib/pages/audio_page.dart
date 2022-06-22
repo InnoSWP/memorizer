ffcd24877948d83bd52202d30cc5f18305b4232f
Sunagatullin <altairsunagatullin@gmail.com>
2022-06-22 12:37:00 +0300

    Tarnsferring text and clear, orientation, code optim

---
+++ lib/pages/audio_page.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:memorizer/settings/constants.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class AudioPlayerOur extends StatefulWidget {
+  final List<String> sentences;
+
+  AudioPlayerOur({required this.sentences, Key? key}) : super(key: key);
+
+  @override
+  State<AudioPlayerOur> createState() => _AudioPlayerOurState();
+}
+
+class _AudioPlayerOurState extends State<AudioPlayerOur> {
+  final ItemScrollController _itemScrollController = ItemScrollController();
+  final ItemPositionsListener _itemPositionsListener =
+      ItemPositionsListener.create();
+  int _currentSentenceIndex = 0;
+
+  bool _isPlayingAudio = false;
+  bool _isLoop = false;
+  IconData playBtnIcon = Icons.play_arrow;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _speechToText = stt.SpeechToText();
+  }
+
+  late stt.SpeechToText _speechToText;
+  bool _isListening = false;
+  String _command = 'Say a command';
+
+  void nextSentence() {
+    if (_currentSentenceIndex != widget.sentences.length - 1) {
+      _currentSentenceIndex++;
+    }
+    playCurrentSentence();
+  }
+
+  void previousSentence() {
+    if (_currentSentenceIndex != 0) _currentSentenceIndex--;
+
+    playCurrentSentence();
+  }
+
+  void playCurrentSentence() {
+    _itemScrollController.scrollTo(
+      index: _currentSentenceIndex,
+      duration: const Duration(
+        milliseconds: 500,
+      ),
+      curve: Curves.easeInOutCubic,
+    );
+  }
+
+  void loopButtonPressed() {
+    setState(() {
+      if (_isLoop == false) {
+        _isLoop = true;
+      } else {
+        _isLoop = false;
+      }
+    });
+  }
+
+  void prevButtonPressed() {
+    setState(() {
+      previousSentence();
+    });
+  }
+
+  void playButtonPressed() {
+    setState(() {
+      if (_isPlayingAudio) {
+        _isPlayingAudio = false;
+        playBtnIcon = Icons.play_arrow;
+      } else {
+        _isPlayingAudio = true;
+        playBtnIcon = Icons.pause;
+      }
+      playCurrentSentence();
+    });
+  }
+
+  void nextButtonPressed() {
+    setState(() {
+      nextSentence();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: kAppBarBackClr,
+        title:
+            Text("AUDIO PLAYER PAGE", style: TextStyle(color: kAppBarTextClr)),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 10,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: kDarkGradientBackground,
+                ),
+                child: ScrollablePositionedList.builder(
+                  itemCount: widget.sentences.length,
+                  itemBuilder: (context, index) => Text(
+                    widget.sentences[index],
+                    style: index == _currentSentenceIndex
+                        ? kTextStyleSelected
+                        : kTextStyleMain,
+                  ),
+                  itemScrollController: _itemScrollController,
+                  itemPositionsListener: _itemPositionsListener,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    onPressed: _listen,
+                    child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.info_outlined),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: kDarkGradientBackground,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      onPressed: loopButtonPressed,
+                      child: const Icon(Icons.repeat),
+                    ),
+                    TextButton(
+                      onPressed: prevButtonPressed,
+                      child: const Icon(Icons.skip_previous),
+                    ),
+                    TextButton(
+                      onPressed: playButtonPressed,
+                      child: Icon(playBtnIcon),
+                    ),
+                    TextButton(
+                      onPressed: nextButtonPressed,
+                      child: const Icon(Icons.skip_next),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speechToText.initialize(
+        onStatus: (val) {
+          if (kDebugMode) {
+            print('onStatus: $val');
+          }
+          if (val == 'done') {
+            _stopListening();
+          }
+        },
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speechToText.listen(
+            onResult: (val) => setState(() {
+                  _command = val.recognizedWords;
+                }));
+      }
+    } else {
+      _stopListening();
+    }
+  }
+
+  void _stopListening() {
+    setState(() {
+      _isListening = false;
+      if (_command.isNotEmpty) {
+        if (_command == 'next') {
+          nextSentence();
+        } else if (_command == 'previous') {
+          previousSentence();
+        } else if (_command == 'play') {
+          playCurrentSentence();
+        }
+      }
+    });
+    _command = '';
+    _speechToText.stop();
+  }
+}
---
+++ lib/pages/input_page.dart
@@ -0,0 +1,200 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:memorizer/settings/constants.dart' as clr;
+import 'package:pdf_text/pdf_text.dart';
+
+import '../modules/my_button.dart';
+import 'audio_page.dart';
+
+class InputText extends StatefulWidget {
+  const InputText({Key? key}) : super(key: key);
+
+  @override
+  State<InputText> createState() => _InputTextState();
+}
+
+class _InputTextState extends State<InputText> {
+  final _inputTextFieldController = TextEditingController();
+  File? file;
+  PDFDoc? pdf;
+  String pdfInput = "";
+  String justInput = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: clr.kAppBarBackClr,
+        title: Text("INPUT PAGE", style: TextStyle(color: clr.kAppBarTextClr)),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TextField(
+                controller: _inputTextFieldController,
+                autocorrect: true,
+                enableInteractiveSelection: true,
+                textCapitalization: TextCapitalization.sentences,
+                enableSuggestions: true,
+                cursorColor: clr.kBnbSelectedItemClr,
+                keyboardAppearance: Brightness.dark,
+                textInputAction: TextInputAction.done,
+                readOnly: false,
+                enabled: true,
+                minLines: 17,
+                maxLines: 17,
+                decoration: InputDecoration(
+                  counterText: "Number of words : 169",
+                  counterStyle: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey.shade500,
+                  ),
+                  contentPadding: const EdgeInsets.all(20),
+                  helperText: 'Input your text and press Memorize!',
+                  helperStyle: const TextStyle(fontSize: 14),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade700,
+                        width: 5,
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        color: clr.kBnbSelectedItemClr,
+                        width: 1,
+                      )),
+                  hintText: "Type the text or upload PDF file...",
+                  hintStyle: TextStyle(
+                    color: clr.kBnbSelectedItemClr,
+                    fontSize: 28,
+                  ),
+                  filled: true,
+                  fillColor: clr.kGrey,
+                ),
+                onChanged: (input) {
+                  setState(() {
+                    justInput = input;
+                  });
+                },
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MyButton(
+                      title: "Clear",
+                      //size: const Size(90, 50),
+                      onPressed: () {
+                        _inputTextFieldController.text = "";
+                        if (kDebugMode) print("clear file");
+                        setState(() {
+                          file = null;
+                          pdf = null;
+                          pdfInput = "";
+                          justInput = "";
+                        });
+                      }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MyButton(
+                    title: "Upload a File",
+                    //size: Size(180, 100),
+                    onPressed: () async {
+                      if (kDebugMode) {
+                        print("Upload a File");
+                      }
+                      FilePickerResult? result =
+                          await FilePicker.platform.pickFiles(
+                        allowMultiple: false,
+                        type: FileType.custom,
+                        allowedExtensions: ['pdf'],
+                      );
+
+                      setState(() {
+                        if (result != null) {
+                          file = File(result.files.single.path!);
+                          if (kDebugMode) {
+                            print('file uploaded successfully');
+                          }
+                        } else {
+                          if (kDebugMode) {
+                            print("result is NULL!!!");
+                          }
+                        }
+                      });
+
+                      setState(() async {
+                        if (file != null) {
+                          pdf = await PDFDoc.fromFile(file!);
+                          pdfInput = (await pdf?.text)!;
+                          if (kDebugMode) {
+                            print(pdfInput);
+                          }
+                        }
+                      });
+                    },
+                  ),
+                  Text(
+                    file != null
+                        ? "Picked File Name: ${file?.path.split('/').last}"
+                        : "No Picked File",
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              MyButton(
+                title: "Memorize",
+                //size: const Size(180, 80),
+                onPressed: () {
+                  if (justInput != "" || pdfInput != "") {
+                    List<String> listOfSentences = <String>[];
+
+                    //'[^\.\!\?]*[\.\!\?]'
+                    if (pdfInput != "") {
+                      listOfSentences = pdfInput
+                          .replaceAll('\n', '')
+                          .split("\\.|\\!|\\?|\\...|\\?!|\\!?|\\!!!|\\???");
+                    } else if (justInput != '') {
+                      listOfSentences = justInput
+                          .replaceAll('\n', '')
+                          .split("\\.|\\!|\\?|\\...|\\?!|\\!?|\\!!!|\\???");
+                    }
+
+                    if (listOfSentences.isEmpty) {
+                      listOfSentences.add("Empty");
+                    }
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                AudioPlayerOur(sentences: listOfSentences)));
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
