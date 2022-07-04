7fea8643b60e6a630a49d14348169e94a52e6f47
kamil <almetov.kamil@gmail.com>
2022-07-04 14:06:12 +0300

    Fix info page

--- lib/pages/info_page.dart
+++ lib/pages/info_page.dart
@@ -12,6 +12,7 @@ class InfoPage extends StatelessWidget {
     var infoTextStyle = TextStyle(
         fontSize: 15.sp,);
     var horizontalMargin = EdgeInsets.symmetric(horizontal: 3.w);
+    var paddingVar = EdgeInsets.symmetric(vertical: 0.8.h);
     return Expanded(
       child: Container(
         decoration: BoxDecoration(
@@ -28,110 +29,125 @@ class InfoPage extends StatelessWidget {
         child: Wrap(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Row(
-              children: [
-                Container(
-                  margin: horizontalMargin,
-                  height: 8.h,
-                  width: 12.w,
-                  child: FloatingActionButton(
-                    heroTag: 'micro info',
-                    mini: false,
-                    onPressed: () {},
-                    child: const Icon(Icons.mic),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'Tap microphone to start a voice command',
-                    style: infoTextStyle,
+            Padding(
+              padding: paddingVar,
+              child: Row(
+                children: [
+                  Container(
+                    margin: horizontalMargin,
+                    height: 8.h,
+                    width: 12.w,
+                    child: FloatingActionButton(
+                      heroTag: 'micro info',
+                      mini: false,
+                      onPressed: () {},
+                      child: const Icon(Icons.mic),
                     ),
                   ),
-              ],
+                  Expanded(
+                    child: Text(
+                      'Tap microphone to start a voice command',
+                      style: infoTextStyle,
+                      ),
+                    ),
+                ],
+              ),
             ),
             Container(
               margin: horizontalMargin,
               child: Align(
                 alignment: Alignment.topLeft,
                 child: Text(
-                  'Current voice commands:\n\u2022 Play!\n\u2022 Stop!\n\u2022 Repeat n times!\n\u2022 Next!\n\u2022 Previous!',
+                  'Current voice commands:\n\u2022 Play!\n\u2022 Stop!\n\u2022 Repeat n times (each)!\n\u2022 Repeat infinitely/off\n\u2022 Next!\n\u2022 Previous!',
                   style: infoTextStyle,
                 ),
               ),
             ),
-            Row(
-              children: [
-                Container(
-                  margin: horizontalMargin,
-                  child: MyButton(
+            Padding(
+              padding: paddingVar,
+              child: Row(
+                children: [
+                  Container(
+                    margin: horizontalMargin,
+                    child: MyButton(
+                        height: kAduioPlayerButtonHeight,
+                        width: kAduioPlayerButtonWidth,
+                        iconData: Icons.skip_previous,
+                        onPressed: () {}),
+                  ),
+                   Expanded(
+                    child: Text(
+                      'Tap to go to the previous sentence',
+                      style: infoTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: paddingVar,
+              child: Row(
+                children: [
+                  Container(
+                    margin: horizontalMargin,
+                    child: MyButton(
+                        height: kAduioPlayerButtonHeight,
+                        width: kAduioPlayerButtonWidth,
+                        iconData: Icons.play_arrow,
+                        onPressed: () {}),
+                  ),
+                   Expanded(
+                    child: Text(
+                      'Tap to start memorizing, tap once more to stop',
+                      style: infoTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: paddingVar,
+              child: Row(
+                children: [
+                  Container(
+                    margin: horizontalMargin,
+                    child: MyButton(
+                        height: kAduioPlayerButtonHeight,
+                        width: kAduioPlayerButtonWidth,
+                        iconData: Icons.skip_next,
+                        onPressed: () {}),
+                  ),
+                   Expanded(
+                    child: Text(
+                      'Tap to go to the next sentence',
+                      style: infoTextStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: paddingVar,
+              child: Row(
+                children: [
+                  Container(
+                    margin: horizontalMargin,
+                    child: MyButton(
                       height: kAduioPlayerButtonHeight,
                       width: kAduioPlayerButtonWidth,
-                      iconData: Icons.skip_previous,
-                      onPressed: () {}),
-                ),
-                 Expanded(
-                  child: Text(
-                    'Tap to go to the previous sentence',
-                    style: infoTextStyle,
+                      onPressed: () {},
+                      iconData: Icons.repeat,
+                      iconColor: Colors.white,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                  margin: horizontalMargin,
-                  child: MyButton(
-                      height: kAduioPlayerButtonHeight,
-                      width: kAduioPlayerButtonWidth,
-                      iconData: Icons.play_arrow,
-                      onPressed: () {}),
-                ),
-                 Expanded(
-                  child: Text(
-                    'Tap to start memorizing, tap once more to stop',
-                    style: infoTextStyle,
+                   Expanded(
+                    child: Text(
+                      'Tap to repeat the current sentence\nLong press to enter repeat settings',
+                      style: infoTextStyle,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                  margin: horizontalMargin,
-                  child: MyButton(
-                      height: kAduioPlayerButtonHeight,
-                      width: kAduioPlayerButtonWidth,
-                      iconData: Icons.skip_next,
-                      onPressed: () {}),
-                ),
-                 Expanded(
-                  child: Text(
-                    'Tap to go to the next sentence',
-                    style: infoTextStyle,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                  margin: horizontalMargin,
-                  child: MyButton(
-                    height: kAduioPlayerButtonHeight,
-                    width: kAduioPlayerButtonWidth,
-                    onPressed: () {},
-                    iconData: Icons.repeat,
-                    iconColor: Colors.white,
-                  ),
-                ),
-                 Expanded(
-                  child: Text(
-                    'Tap to repeat the current sentence',
-                    style: infoTextStyle,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
 
           ],
